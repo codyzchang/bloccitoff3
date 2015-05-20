25e6ef03783493ac25f490b3de3daa030913dbe8
Cody Chang <codyzchang@gmail.com>
2015-05-20 16:22:35 +0000

    added item HTML CSS

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150520151540) do
+ActiveRecord::Schema.define(version: 20150520154737) do
 
   create_table "items", force: :cascade do |t|
     t.string   "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
+
+  add_index "items", ["user_id"], name: "index_items_on_user_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,33 @@
  require 'faker'
  
- # Create Posts
+5.times do
+   user = User.new(
+     name:     Faker::Name.name,
+     email:    Faker::Internet.email,
+     password: Faker::Lorem.characters(10)
+   )
+   user.skip_confirmation!
+   user.save!
+ end
+ users = User.all
+
+# Create Items
  50.times do
    Item.create!(
+     user:   users.sample,
      title:  Faker::Lorem.sentence,
-     body:   Faker::Lorem.paragraph
+
    )
  end
 items = Item.all
- 
 
- puts "Seed finished"
+ user = User.first
+ user.skip_reconfirmation!
+ user.update_attributes!(
+   email: 'james@sharklasers.com',
+   password: 'helloworld'
+ )
+
+puts "Seed finished"
+puts "#{User.count} users created"
 puts "#{Item.count} posts created"
